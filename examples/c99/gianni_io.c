2feb3caa6a39e7427026a6b68e9ac2d4df0c844d
giadarol <giovanni.iadarola@cern.ch>
2018-11-21 13:41:31 +0100

    I manage to make a copy

--- examples/c99/gianni_io.c
+++ examples/c99/gianni_io.c
@@ -12,13 +12,23 @@ int main( int argc, char* argv[] )
 
   printf("Hello world!\n");
 
+  // Load particles
   st_Buffer* input_pb = st_Buffer_new_from_file( "particles.buffer" );
   SIXTRL_ASSERT( input_pb != SIXTRL_NULLPTR );
 
-  st_Particles const* in_particles =
-      st_Particles_buffer_get_const_particles( input_pb, 0u );
+  st_Particles* particles =
+      st_Particles_buffer_get_particles( input_pb, 0u );
 
-  st_Particles_print_out( in_particles );
+  st_Particles_print_out( particles );
+
+
+
+
+
+  st_Buffer* outp_buffer = st_Buffer_new( 0u );
+  st_Particles_add_copy( outp_buffer, particles );
+
+  st_Buffer_write_to_file( outp_buffer, "outp.buffer" );
 
 
 
